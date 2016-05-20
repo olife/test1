c1875bf115af9e58e8f1b4a5b45d35c83ca6550c
olife <294611470@qq.com>
2016-05-20 17:39:54 +0800

    test5

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Log.i("TAG", "-------test---4--------");
+        Log.i("TAG", "-------test---5--------");
     }
 
     @Override
